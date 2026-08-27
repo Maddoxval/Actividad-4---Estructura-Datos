0c012be6f7c2310a20b3b10be5d96ffba8a7ccdc
Maddoxval <maddox03128@gmail.com>
2026-08-27 08:41:25 -0500

    Create EstacionMonitoreo.java

---
+++ EstacionMonitoreo.java
@@ -0,0 +1,16 @@
+class EstacionMonitoreo {
+
+    private String nombre;
+
+    public EstacionMonitoreo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void monitorear(Sensor sensor) {
+        System.out.println(
+            "Sensor: " + sensor.getId()
+            + " | Lectura: "
+            + sensor.leer()
+        );
+    }
+}
