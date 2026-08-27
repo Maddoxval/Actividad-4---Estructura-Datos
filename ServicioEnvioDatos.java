4193b675c8dde889e4dc77fb78f516ac8e2ce139
Maddoxval <maddox03128@gmail.com>
2026-08-27 08:41:56 -0500

    Create ServicioEnvioDatos.java

---
+++ ServicioEnvioDatos.java
@@ -0,0 +1,11 @@
+class ServicioEnvioDatos {
+
+    public void enviar(String sensorId, double valor) {
+        System.out.println(
+            "Enviando lectura de "
+            + sensorId
+            + ": "
+            + valor
+        );
+    }
+}
