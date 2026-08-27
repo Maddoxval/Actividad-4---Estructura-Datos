a0dfa70df700cf0abefc0d2c84b14c9cb7f163c3
Maddoxval <maddox03128@gmail.com>
2026-08-27 08:38:41 -0500

    Create SensorTemperatura.java

---
+++ SensorTemperatura.java
@@ -0,0 +1,11 @@
+class SensorTemperatura extends Sensor {
+
+    public SensorTemperatura(String id, String ubicacion) {
+        super(id, ubicacion);
+    }
+
+    @Override
+    public double leer() {
+        return 24.5;
+    }
+}
