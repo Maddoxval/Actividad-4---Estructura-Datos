d28e13d130717b77852133c2cc886ff374b54b47
Maddoxval <maddox03128@gmail.com>
2026-08-27 08:39:31 -0500

    Create SensorCalidadAire.java

---
+++ SensorCalidadAire.java
@@ -0,0 +1,11 @@
+class SensorCalidadAire extends Sensor {
+
+    public SensorCalidadAire(String id, String ubicacion) {
+        super(id, ubicacion);
+    }
+
+    @Override
+    public double leer() {
+        return 42.0;
+    }
+}
